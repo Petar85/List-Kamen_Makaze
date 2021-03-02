0872747bb5470234feb2bf81d2eff5cd730891dd
Peter Pan <petarzivkovic85@gmail.com>
2021-03-02 02:31:34 -0800

    created a function that return random choice

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const userScore = 0;
 const computerScore = 0;
-const = userScore_span = document.getElementById("user-score");
-const = computerScore_span = document.getElementById("computer-score");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices [randomNumber];
+}
